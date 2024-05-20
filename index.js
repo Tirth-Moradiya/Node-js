af4cc923d8999517243068a81f3188c69fd59b06
Tirth-Moradiya <107025172+Tirth-Moradiya@users.noreply.github.com>
2024-05-20 15:03:22 +0530

    add node_module files

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const http = require("http");
+const Color = require("colors");
 
-console.log("create rep");
+http
+  .createServer((req, res) => {
+    res.write("<h1>this is demo for create server</h1>");
+    res.end();
+  })
+  .listen(4500);
