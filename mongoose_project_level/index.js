c7d9a6ce8bfe5090b9323007e55d6175ee6b54df
Tirth-Moradiya <107025172+Tirth-Moradiya@users.noreply.github.com>
2024-06-20 18:44:01 +0530

    x

--- mongoose_project_level/index.js
+++ mongoose_project_level/index.js
@@ -12,4 +12,32 @@ app.post("/create", async (req, resp) => {
   resp.send(result);
 });
 
+app.get("/list", async (req, resp) => {
+  let data = await Student.find();
+  resp.send(data);
+});
+
+app.delete("/delete/:_id", async (req, resp) => {
+  console.log(req.params);
+  let data = await Student.deleteOne(req.params);
+  resp.send(data);
+});
+
+app.put("/update/:_id", async (req, resp) => {
+  console.log(req.params);
+  let data = await Student.updateOne(req.params, { $set: req.body });
+  resp.send(data);
+});
+
+app.get("/search/:key", async (req, resp) => {
+  console.log(req.params.key);
+  let data = await Student.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { gender: { $regex: req.params.key } },
+    ],
+  });
+  resp.send(data);
+});
+
 app.listen(2200);
